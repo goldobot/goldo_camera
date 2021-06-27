c4d4303f21b6363785e9d48c4ee701a31cf4ee2a
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2021-06-27 10:33:47 +0200

    Test harbor: filter false positive.

--- analyse_image_test.py
+++ analyse_image_test.py
@@ -69,30 +69,26 @@ class TestAnalyseImage(unittest.TestCase):
         #anl.debug = True # Uncomment to see all images
         #anl.color_mask = True # Uncomment to see only images with color mask
         #anl.check = True # Uncomment to see only final images
-        detected_shapes = anl.analyse_image(image, 200, cache=harbor_cache, scale_percent=0.5, crop_percent=0.3)
+        detected_shapes = anl.analyse_image(image, 2000, cache=harbor_cache, scale_percent=0.5, crop_percent=0.3)
         for shape in detected_shapes:
             print("detected shapes", shape)
-        self.assertEqual(len(detected_shapes), 10, "Should be 10")
+        self.assertEqual(len(detected_shapes), 8, "Should be 8")
         self.assertEqual(detected_shapes[0].color, "red", "Should be red")
         self.assertEqual(detected_shapes[1].color, "green", "Should be green")
         self.assertEqual(detected_shapes[2].color, "red", "Should be red")
         self.assertEqual(detected_shapes[3].color, "red", "Should be red")
-        self.assertEqual(detected_shapes[4].color, "green", "Should be green")
-        self.assertEqual(detected_shapes[5].color, "red", "Should be red")
-        self.assertEqual(detected_shapes[6].color, "red", "Should be red")
+        self.assertEqual(detected_shapes[4].color, "red", "Should be red")
+        self.assertEqual(detected_shapes[5].color, "green", "Should be green")
+        self.assertEqual(detected_shapes[6].color, "green", "Should be green")
         self.assertEqual(detected_shapes[7].color, "green", "Should be green")
-        self.assertEqual(detected_shapes[8].color, "green", "Should be green")
-        self.assertEqual(detected_shapes[9].color, "green", "Should be green")
         self.assertTrue(detected_shapes[0].up)
         self.assertTrue(detected_shapes[1].up)
         self.assertFalse(detected_shapes[2].up)
         self.assertFalse(detected_shapes[3].up)
-        self.assertFalse(detected_shapes[4].up)
-        self.assertFalse(detected_shapes[5].up)
-        self.assertTrue(detected_shapes[6].up)
+        self.assertTrue(detected_shapes[4].up)
+        self.assertTrue(detected_shapes[5].up)
+        self.assertFalse(detected_shapes[6].up)
         self.assertTrue(detected_shapes[7].up)
-        self.assertFalse(detected_shapes[8].up)
-        self.assertTrue(detected_shapes[9].up)
 
 if __name__ == '__main__':
     unittest.main()
