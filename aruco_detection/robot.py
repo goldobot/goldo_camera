3d958cef6c8eefc0c775a775b835f3644464adfe
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2021-12-02 21:58:46 +0100

    uX -> xX.

--- aruco_detection/robot.py
+++ aruco_detection/robot.py
@@ -34,7 +34,7 @@ def main():
         if data == 'q':
             break
         data.ParseFromString(raw)
-        print('  Detected ArUco marker: ID={}, c=({}, {}), u=({}, {}), v=({}, {})'.format(data.markerID, data.cX, data.cY, data.uX, data.uY, data.vX, data.vY))
+        print('  Detected ArUco marker: ID={}, c=({}, {}), u=({}, {}), v=({}, {})'.format(data.markerID, data.cX, data.cY, data.xX, data.uY, data.vX, data.vY))
 
 # Main program.
 if __name__ == '__main__':
