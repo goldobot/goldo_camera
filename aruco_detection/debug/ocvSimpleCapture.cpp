40f43d424df6aeb77ff56b6a7f433647309d20b2
Franck HOUSSEN <fghoussen@users.noreply.github.com>
2021-12-14 09:21:23 +0100

    Debug: add timers.

--- aruco_detection/debug/ocvSimpleCapture.cpp
+++ aruco_detection/debug/ocvSimpleCapture.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
 #include <cstdlib>
+#include <chrono>
 
 using namespace std;
 using namespace cv;
@@ -8,8 +9,8 @@ using namespace cv;
 int main(int argc, char ** argv) {
 
   // Create a VideoCapture object
-  int vidID = argc == 1 ? 0 : atoi(argv[1]);
-  VideoCapture cap(vidID); 
+  int vidID = argc == 2 ? atoi(argv[1]) : 0;
+  VideoCapture cap(vidID);
 
   // Check if camera opened successfully
   if (!cap.isOpened()) {
@@ -19,18 +20,26 @@ int main(int argc, char ** argv) {
 
   while (1) {
     // Capture frame-by-frame
-    Mat frame;
-    cap >> frame;
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+    Mat frame; cap >> frame;
+    std::chrono::steady_clock::time_point stop = std::chrono::steady_clock::now();
+    auto timeFrame = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
 
     // If the frame is empty, break immediately
     if (frame.empty()) break;
 
     // Display the resulting frame
+    start = std::chrono::steady_clock::now();
     imshow( "Frame", frame );
+    stop = std::chrono::steady_clock::now();
+    auto timeImshow = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+
+    // Print timing.
+    cout << "timeFrame " << timeFrame/1000. << " s, timeImshow " << timeImshow/1000. << "s" << endl;
 
     // Press ESC on keyboard to exit
     char c = (char) waitKey(25);
-    if (c==27) break;
+    if (c == 'q') break;
   }
 
   // When everything done, release the video capture object
--- aruco_detection/debug/ocvSimpleCapture.py
+++ aruco_detection/debug/ocvSimpleCapture.py
@@ -1,22 +1,29 @@
-# import the opencv library
+# Import library
 import cv2
 import sys
+import time
 
-# define a video capture object
-vidID = 0 if len(sys.argv) == 1 else int(sys.argv[1])
+# Define a video capture object
+vidID = int(sys.argv[1]) if len(sys.argv) == 2 else 0
 vid = cv2.VideoCapture(vidID)
 
 while(True):
-    # Capture the video frame
-    # by frame
+    # Capture the video frame by frame
+    start = time.time()
     ret, frame = vid.read()
+    stop = time.time()
+    timeFrame = stop - start
 
     # Display the resulting frame
+    start = time.time()
     cv2.imshow('frame', frame)
+    stop = time.time()
+    timeImshow = stop - start
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
+    # Print timing.
+    print('timeFrame %.3f s, timeImshow %.3f s'%(timeFrame, timeImshow))
+
+    # Press 'q' to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
